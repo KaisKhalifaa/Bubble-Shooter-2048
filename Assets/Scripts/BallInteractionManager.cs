3fc6a28608eb3626666ad957745cdba9db0be1b1
KaisKhalifaa <khalifa.kais.it@gmail.com>
2024-04-02 09:48:12 +0100

    7ases famma aghlat f coroutines since awel mara net3amel maahom ama works fine, ne9sa chway hajet

--- Assets/Scripts/BallInteractionManager.cs
+++ Assets/Scripts/BallInteractionManager.cs
@@ -6,42 +6,50 @@ using System.Linq;
 using System;
 public class BallInteractionManager : MonoBehaviour
 {   
+        public static BallInteractionManager Instance { get; private set; }
+
     [SerializeField] BallShooter _ballShooter;
-    GameObject _ball;
+     GameObject _ball;
     [SerializeField] Transform objectPoolGameObject;
     bool _ballVisited = true;
     [SerializeField] List<GameObject> _connectedBalls = new List<GameObject>();
-    static bool _coroutineIsFinished = false;
+    static bool _coroutineIsFinished = false, _checker;
 
-    [SerializeField] GameObject ball1, ball2;
+    static GameObject _resultingBall;
     
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+    }
     
     void Start()
     {
         _ballShooter = BallShooter.Instance;
-        
     }
     void Update()
     {
         if (_ballShooter.BallArrived)
         {
             _ball = _ballShooter.BallToShoot;
-
-            if (HasSimilarNeighbor(_ball))
-            {
-                PutSimilarConnectedBallsInList(_ball);
-                StartCoroutine(AnimateConnectedBalls(int.Parse(_ball.tag)));
-            }
-
+            StartCoroutine(BallAnimationRepeater(_ball));
             _ballShooter.BallArrived = false;
         }
-
-        if (Input.GetKeyDown(KeyCode.G))
-        {
-            StartCoroutine(MoveCoroutine(ball1, ball2.transform.position, 0.5f));
-        }
+        
     }
     
+    IEnumerator BallAnimationRepeater(GameObject ball)
+    {
+        if (HasSimilarNeighbor(ball))
+        {
+            PutSimilarConnectedBallsInList(ball);
+            yield return StartCoroutine(AnimateConnectedBalls(int.Parse(ball.tag)));
+            StartCoroutine(BallAnimationRepeater(_resultingBall));
+        }
+    }
+
     bool HasSimilarNeighbor(GameObject ball) 
     {
         float ballCollisionRadius = ball.GetComponent<CircleCollider2D>().radius + 0.4f;
@@ -78,15 +86,63 @@ public class BallInteractionManager : MonoBehaviour
             continue;
         }
 
-        //ballComponent.Visited = false;
         _connectedBalls.Add(ball);
         //Debug.Log("ball added to list");
-        // ball.SetActive(false);
-        // ball.transform.SetParent(objectPoolGameObject);
-        
     }
 
-    // IEnumerator AnimateConnectedBalls()
+    IEnumerator AnimateConnectedBalls(int ballTagNumber)
+    {
+        _coroutineIsFinished = false;
+        List<Coroutine> coroutines = new List<Coroutine>();
+        GameObject lastBallInConnectedBalls = _connectedBalls[0];
+        // Start all coroutines simultaneously
+        foreach (GameObject ball in _connectedBalls)
+        {   
+            Coroutine coroutine = StartCoroutine(MoveCoroutine(ball, lastBallInConnectedBalls.transform.position, 0.3f));
+            coroutines.Add(coroutine);//el wa9t eli nhottou supposé lwakt ta l transition f 7arket l balls!!!!!!
+        }
+
+        // Wait for all coroutines to finish
+        foreach (Coroutine coroutine in coroutines)
+        {
+            yield return coroutine;
+        }
+
+        // After all coroutines have finished, perform additional actions
+        foreach (GameObject ball in _connectedBalls)
+        {
+            ball.SetActive(false);
+            ball.transform.SetParent(objectPoolGameObject);
+            ball.GetComponent<Ball>().Visited = false;
+        }
+
+            //look for the ball to replace the previously connected Balls
+            double connectedBallsAmount =_connectedBalls.Count;
+            int resultingBallTag = (int)(ballTagNumber * Math.Pow(2, connectedBallsAmount-1));
+            _resultingBall = ObjectPool._instance.GetPooledBallByTag(resultingBallTag);
+            ObjectPool._instance.ActivateAndSetPooledObjectPosition(_resultingBall, lastBallInConnectedBalls.transform.position, 1f);
+            //resultingBall.transform.SetParent(BallContainer.transform);
+
+            // Clear the list
+            _connectedBalls.Clear();
+            _coroutineIsFinished = true;
+        }
+
+    private IEnumerator MoveCoroutine(GameObject obj, Vector3 targetPosition, float duration)
+    {
+        float timeElapsed = 0f;
+        while (timeElapsed < duration)
+        {
+            timeElapsed += Time.deltaTime;
+            float t = Mathf.Clamp01(timeElapsed / duration);
+            obj.transform.position = Vector3.Lerp(obj.transform.position, targetPosition, t);
+            yield return null;
+        }
+        obj.transform.position = targetPosition; // Ensure final position is exact
+    }
+}
+
+ // IEnumerator AnimateConnectedBalls()
     // {
     //     for (int i = 0; i<= _connectedBalls.Count-1; i++)
     //     {
@@ -97,57 +153,3 @@ public class BallInteractionManager : MonoBehaviour
     //     }
     //     _connectedBalls.Clear();
     // }
-
-    IEnumerator AnimateConnectedBalls(int ballTagNumber)
-    {
-    List<Coroutine> coroutines = new List<Coroutine>();
-    GameObject lastBallInConnectedBalls = _connectedBalls[0];
-    // Start all coroutines simultaneously
-    foreach (GameObject ball in _connectedBalls)
-    {
-        
-        Coroutine coroutine = StartCoroutine(MoveCoroutine(ball, lastBallInConnectedBalls.transform.position, 0.2f));
-        coroutines.Add(coroutine);
-    }
-
-    // Wait for all coroutines to finish
-    foreach (Coroutine coroutine in coroutines)
-    {
-        yield return coroutine;
-    }
-
-    // After all coroutines have finished, perform additional actions
-    foreach (GameObject ball in _connectedBalls)
-    {
-        ball.SetActive(false);
-        ball.transform.SetParent(objectPoolGameObject);
-        ball.GetComponent<Ball>().Visited = false;
-    }
-
-    //look for the ball to replace the previously connected Balls
-        double connectedBallsAmount =_connectedBalls.Count;
-        int resultingBallTag = (int)(ballTagNumber * Math.Pow(2, connectedBallsAmount-1));
-        GameObject resultingBall = ObjectPool._instance.GetPooledBallByTag(resultingBallTag);
-        ObjectPool._instance.ActivateAndSetPooledObjectPosition(resultingBall, lastBallInConnectedBalls.transform.position, 1f);
-        //resultingBall.transform.SetParent(BallContainer.transform);
-
-        // Clear the list
-        _connectedBalls.Clear();
-    }
-
-    private IEnumerator MoveCoroutine(GameObject obj, Vector3 targetPosition, float duration)
-    {
-        _coroutineIsFinished = false;
-        float timeElapsed = 0f;
-        while (timeElapsed < duration)
-        {
-            timeElapsed += Time.deltaTime;
-            float t = Mathf.Clamp01(timeElapsed / duration);
-            obj.transform.position = Vector3.Lerp(obj.transform.position, targetPosition, t);
-            yield return null;
-        }
-        obj.transform.position = targetPosition; // Ensure final position is exact
-        _coroutineIsFinished = true;
-    }
-}
-
